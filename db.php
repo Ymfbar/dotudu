595c10f04d3a3a0e662a72f4d24308b0b269e8cb
WIRANTI OKTAVIANI TANWIN ZHI <wirantioktavianitanwin@gmail.com>
2025-06-22 21:22:35 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,13 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db = "dotudu_db";
+$host = "sql303.infinityfree.com";       // Host dari cPanel
+$user = "if0_39295605";                  // Username MySQL
+$pass = "kelompokbarang";               // Password MySQL
+$db   = "if0_39295605_dotudu_db";        // Nama database di InfinityFree
 
 $koneksi = new mysqli($host, $user, $pass, $db);
 
 if ($koneksi->connect_error) {
     die("Koneksi gagal: " . $koneksi->connect_error);
 }
-
 ?>
